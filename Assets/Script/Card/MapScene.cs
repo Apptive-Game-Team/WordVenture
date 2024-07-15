9f30a6c9db449f2bc213407257c628bd92403774
191012 <kimhj191012@naver.com>
2024-07-15 16:42:40 +0900

    Change ShowBattle, ShowWordPosition  (using Dictionary)

--- Assets/Script/Card/MapScene.cs
+++ Assets/Script/Card/MapScene.cs
@@ -25,11 +25,15 @@ namespace Map_scene
         int position = 0;
         public static int StagePosition;
         Dictionary<int,System.Action> StageMove;
+        Dictionary<int,System.Action> BattleBackground;
+        Dictionary<int,System.Action> WordPosition;
 
         private void Start()
         {
-            InitShowBattles();
             InitStageMove();
+            InitBattleBackground();
+            InitWordPosition();
+            InitShowBattles();
         }
 
         void Update()
@@ -107,6 +111,30 @@ namespace Map_scene
                         }
                     }
                 }
+            };
+        }
+
+        void InitBattleBackground()
+        {
+            BattleBackground = new Dictionary<int, System.Action>
+            {
+                {0, () => {GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage1;}},
+                {1, () => {GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage2;}},
+                {2, () => {GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage3;}},
+                {3, () => {GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;}},
+                {4, () => {GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;}}
+            };
+        }
+
+        void InitWordPosition()
+        {
+            WordPosition = new Dictionary<int, System.Action>
+            {
+                {1, () => {character.transform.position = battle1.transform.position;}},
+                {2, () => {character.transform.position = battle2.transform.position;}},
+                {3, () => {character.transform.position = battle3.transform.position;}},
+                {4, () => {character.transform.position = boss.transform.position;}},
+                {5, () => {character.transform.position = boss.transform.position;}},
             };
         }
 
@@ -193,58 +221,60 @@ namespace Map_scene
             {
                 ShowBattle(i);
             }
-            WordPosition(StagePosition);
+            ShowWordPosition(StagePosition);
         }
 
         void ShowBattle(int StagePosition)
         {
-            switch (StagePosition)
-            {
-                case 1:
-                    GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage1;
-                    break;
-                case 2:
-                    GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage2;
-                    break;
-                case 3:
-                    GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage3;
-                    break;
-                case 4:
-                    GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;
-                    break;
-                case 5:
-                    GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;
-                    break;
-                default:
-                    break;
-            }
+            // switch (StagePosition)
+            // {
+            //     case 1:
+            //         GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage1;
+            //         break;
+            //     case 2:
+            //         GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage2;
+            //         break;
+            //     case 3:
+            //         GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage3;
+            //         break;
+            //     case 4:
+            //         GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;
+            //         break;
+            //     case 5:
+            //         GameObject.Find("Background").GetComponent<SpriteRenderer>().sprite = Stage4;
+            //         break;
+            //     default:
+            //         break;
+            // }
+            BattleBackground[StagePosition]();
         }
 
-        void WordPosition(int StagePosition)
+        void ShowWordPosition(int StagePosition)
         {
             position = StagePosition;
-            switch (StagePosition)
-            {
-                case 0:
-                    break;
-                case 1:
-                    character.transform.position = battle1.transform.position;
-                    break;
-                case 2:
-                    character.transform.position = battle2.transform.position;
-                    break;
-                case 3:
-                    character.transform.position = battle3.transform.position;
-                    break;
-                case 4:
-                    character.transform.position = boss.transform.position;
-                    break;
-                case 5:
-                    character.transform.position = boss.transform.position;
-                    break;
-                default:
-                    break;
-            }
+            // switch (StagePosition)
+            // {
+            //     case 0:
+            //         break;
+            //     case 1:
+            //         character.transform.position = battle1.transform.position;
+            //         break;
+            //     case 2:
+            //         character.transform.position = battle2.transform.position;
+            //         break;
+            //     case 3:
+            //         character.transform.position = battle3.transform.position;
+            //         break;
+            //     case 4:
+            //         character.transform.position = boss.transform.position;
+            //         break;
+            //     case 5:
+            //         character.transform.position = boss.transform.position;
+            //         break;
+            //     default:
+            //         break;
+            // }
+            WordPosition[StagePosition]();
         }
 
         void Clear()
